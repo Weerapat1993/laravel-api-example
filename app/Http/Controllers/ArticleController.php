c5a696c1f08e0a9833e45a534901120ff343b26a
Top <weerapat.thawatchoketawee@gmail.com>
2018-03-03 23:47:27 +0700

    Move Comment to php Controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,5 +1,49 @@
 <?php
 
+/**
+ * @api {get} /articles GET Article List
+ * @apiSampleRequest /api/articles
+ * @apiName articleList
+ * @apiGroup Article
+ */
+
+/**
+ * @api {get} /articles/:id GET Article By ID
+ * @apiSampleRequest /api/articles/1
+ * @apiName articleByID
+ * @apiGroup Article
+ */
+
+/**
+ * @api {post} /articles POST Article Create
+ * @apiSampleRequest /api/articles
+ * @apiParam {String} title  Title Stroy
+ * @apiParam {String} description  Description
+ * @apiParam {String} user_id     User ID
+ * @apiName articleCreate
+ * @apiGroup Article
+ */
+
+
+/**
+ * @api {put} /articles PUT Article Update
+ * @apiSampleRequest /api/articles
+ * @apiParam {Number} id  Article ID
+ * @apiParam {String} [title]  Title Stroy
+ * @apiParam {String} [description]  Description
+ * @apiParam {String} [user_id]     User ID
+ * @apiName articleUpdate
+ * @apiGroup Article
+ */
+
+/**
+ * @api {delete} /articles DELETE Article Delete
+ * @apiSampleRequest /api/articles
+ * @apiParam {Number} id  Article ID
+ * @apiName articleDelete
+ * @apiGroup Article
+ */
+
 namespace App\Http\Controllers;
 
 use Exception;
